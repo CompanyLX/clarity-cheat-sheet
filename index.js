ac9f0f3ff48343dae15b3b1e4e46616ddf0bcff2
Kay Anar <43936814+c0d3rk1d@users.noreply.github.com>
2022-10-18 00:50:15 -0400

    copy script

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const fs = require("fs");
 const path = require("path");
 
 const buildDir = "./build";
-const dataDir = "./data";
+const jsDir = "./js";
+const cssDir = "./css";
+const iconDir = "./favicon";
+const contentDir = "./content";
 
-const dataInDir = fs
-  .readdirSync(dataDir)
+const copy = (source, extentions, destination) => {
+  const files = fs
+    .readdirSync(source)
+    .filter((file) =>
+      [...extentions].includes(path.extname(file).substring(1))
+    );
+  files.forEach((file) => {
+    fs.copyFileSync(path.join(source, file), path.join(destination, file));
+  });
+};
+
+const content = [];
+const jsonFiles = fs
+  .readdirSync(contentDir)
   .filter((file) => path.extname(file) === ".json");
-
-const data = [];
-dataInDir.forEach((file) => {
-  const fileData = fs.readFileSync(path.join(dataDir, file));
-  data.push(JSON.parse(fileData.toString()));
+jsonFiles.forEach((file) => {
+  const fileContent = fs.readFileSync(path.join(contentDir, file));
+  content.push(JSON.parse(fileContent.toString()));
 });
 
 fs.rmdirSync(buildDir, { recursive: true, force: true });
@@ -21,4 +34,10 @@ fs.mkdirSync(buildDir);
 
 const template = handlebars.compile(fs.readFileSync("./template.hbt", "utf8"));
 
-fs.writeFileSync(`${buildDir}/index.html`, template(data));
+fs.writeFileSync(`${buildDir}/index.html`, template(content));
+
+copy(jsDir, ["js"], buildDir);
+
+copy(cssDir, ["css"], buildDir);
+
+copy(iconDir, ["png", "xml", "ico", "json"], buildDir);
